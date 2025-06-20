dc530d8f7cfca620ecd6bdeb9f598b49c0b7b271
Saif Malik <infernuspov@gmail.com>
2025-06-20 04:26:08 -0700

    Mobile friendly final innovations

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import "./Navbar.css";
 
 function NavBar() {
+const navigate =useNavigate();
 const scrollTo = (id) => {
   const el = document.getElementById(id);
   if (el) el.scrollIntoView({ behavior: "smooth" });
@@ -17,17 +19,24 @@ const scrollTo = (id) => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return (
+   const Handle1 = () =>{
+  navigate('/') 
+  }
+const Handle2 = () =>{
+  navigate('/contact')
+}
+return(
     <nav className={`navbar ${isScrolled ? "navbar-scrolled" : ""}`}>
       <div className="navbar-inner">
         <ul className="nav-links">
-          <li onClick={() => scrollTo("projects")}>Projects</li>
-          <li onClick={() => scrollTo("novels")}>Novels</li>
-          <li><Link to="/contact" className="nav-link">Contact</Link></li>
+          <li onClick={Handle1}>Home</li>
+          <li onClick={() => scrollTo("projects")} >Projects</li>
+          <li onClick={() => scrollTo("novels")} >Novels</li>
+          <li onClick={Handle2}>Contact</li>
         </ul>
       </div>
     </nav>
-  );
+)
 }
 
 export default NavBar;
